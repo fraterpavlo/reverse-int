22ab7fe9cbeb8d78ead3ff3e01b15f3f11521fc8
fraterpavlo <shukan1999@mail.ru>
2021-03-12 15:14:35 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        result = +(n.toString().split('').reverse().join(''));
+      }
+      else {
+        result = Math.abs(n).toString().split('').reverse().join('');
+      }
+        return result;
 }
+
